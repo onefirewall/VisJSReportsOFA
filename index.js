bf30944521d9ffd5957153553ee37cbeb8e4111f
Sotiraki Sima <sotiraki.sima@onefirewall.com>
2020-04-06 15:51:40 +0100

    dd

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var elasticsearch=require('elasticsearch');
+const ipInt = require('ip-to-int');
 
 var app = express();
 
@@ -61,11 +62,55 @@ function count_count(object_ram, cnt, items){
     );
 }
 
+function return_3d_data(object_ram){
+    client.search({  
+        index: 'ofa-ips',
+        type: '_doc',
+        body: {
+          query: {
+            range : {
+                event_ts : {
+                    gte : "now-60m",
+                    lt :  "now"
+                }
+            }
+          },
+          size: 10000
+        }
+      },function (error, response,status) {
+          if (error){ object_ram.res.status(500).send() }
+          else {
+            items = [  ]
+            response.hits.hits.forEach(function(hit){
+                rand_num = Math.floor(Math.random() * Math.floor(2589598837))
+                
+                try{
+                    range = parseInt(100 + ((parseInt(hit._source.live_score) - 100) / 3))
+                    obj = {x:ipInt(hit._source.src_ip).toInt(),z:parseInt(hit._source.live_score),y:rand_num,style:range}
+                    if(obj.z>100){
+                        items.push(obj)
+                    }
+                }catch(e){
+        
+                }
+            })
+            object_ram.res.send(items)
+          }
+      })
+}
+
 app.get('/api/v1/last_day_data', function (req, res) {
     res.set('Access-Control-Allow-Origin', '*');
 
     var object_ram = {req, res}
     count_count(object_ram, 24, [])
+});
+
+app.get('/api/v1/3d_data', function (req, res) {
+    res.set('Access-Control-Allow-Origin', '*');
+
+    var object_ram = {req, res}
+    return_3d_data(object_ram, 24, [])
 });
 
 app.listen(3000, function () {
--- test.js
+++ test.js
@@ -34,21 +34,20 @@ client.search({
         console.log("search error: "+error)
       }
       else {
-        console.log("--- Response ---");
-        //console.log(response);
-        console.log((response.hits.hits.length))
-        console.log("--- Hits ---");
-        var random = Math.random
         items = [  ]
+        console.log(response.hits.hits.length)
         console.log(Math.floor(Math.random() * Math.floor(100)))
         response.hits.hits.forEach(function(hit){
-          rand_num = Math.floor(Math.random() * Math.floor(1000000))
-          obj = {x:ipInt(hit._source.src_ip).toInt(),z:parseInt(hit._source.live_score),y:rand_num,style:1}
-          if(obj.z>70){
-            items.push(obj)
-          }
+          rand_num = Math.floor(Math.random() * Math.floor(2589598837))
+          try{
+              range = parseInt(100 + ((parseInt(hit._source.live_score) - 100) / 3))
+              obj = {x:ipInt(hit._source.src_ip).toInt(),z:parseInt(hit._source.live_score),y:rand_num,style:range}
+              if(obj.z>100){
+                items.push(obj)
+              }
+          }catch(e){
 
-          //console.log(ipInt(hit._source.src_ip).toInt() + " " + parseInt(hit._source.live_score))
+          }
         })
         console.log(items)
       }
